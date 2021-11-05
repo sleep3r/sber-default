951d27d9019c7ee51ba6584d2bd8c0370b28e4f5
sleep3r <sleep3r@icloud.com>
2021-11-06 02:28:26 +0300

    Initial pipeline

--- config.py
+++ config.py
@@ -84,18 +84,18 @@ def fit(**kwargs) -> CommentedMap:
 def object_from_dict(d: CfgDict, parent=None, **default_kwargs):
     kwargs = dict(d).copy()
     object_type = kwargs.pop("type")
-    params = kwargs.pop("params", {})
+    params = kwargs.pop("params", None)
 
     for name, value in default_kwargs.items():
         params.setdefault(name, value)
     if parent is not None:
-        if params:
+        if params is not None:
             return getattr(parent, object_type)(**params)
         else:
-            return getattr(parent, object_type)(**params)
+            return getattr(parent, object_type)
     else:
         try:
-            if params:
+            if params is not None:
                 return pydoc.locate(object_type)(**params)
             else:
                 return pydoc.locate(object_type)
