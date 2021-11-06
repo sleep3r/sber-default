fd2b306e61f997fc63c36c61cb0f1c4adfbe9d1d
sleep3r <sleep3r@icloud.com>
2021-11-06 13:22:12 +0300

    Initial pipeline

--- train.py
+++ train.py
@@ -176,7 +176,7 @@ def train_model(cfg: MLConfig):
         cat_features=cfg.preprocessing.cat_features,
         verbose=cv_params.verbose,
         k_fold_fn=object_from_dict(cv_params.k_fold_fn),
-        groups_for_split=X_generated_preprocessed[cv_params.groups_col] if cv_params.groups_col else None
+        groups_for_split=X_generated[cv_params.groups_col] if cv_params.groups_col else None
     )
     cv_score, train_score, train_score_std, train_predictions, test_predictions = cv.run()
     meta["metrics"]["CV_score"] = cv_score.mean()
