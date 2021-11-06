978d9b015180c88464388f05ed1469cba8298cc6
sleep3r <sleep3r@icloud.com>
2021-11-06 13:46:54 +0300

    Initial pipeline

--- preprocessing.py
+++ preprocessing.py
@@ -84,6 +84,7 @@ class DefaultTransformer:
             self.X_test = self.X_test.replace({np.inf: self.cfg.preprocessing.replace_inf}).copy()
 
         if self.cfg.preprocessing.select_rows is not None:
+            print(self.X.columns)
             self.X = self.X[self.X[self.cfg.preprocessing.select_rows] is True].copy()
 
         if self.cfg.preprocessing.process_na == "drop":
