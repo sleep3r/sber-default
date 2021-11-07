b1012c309e06d56f011deeabdff67b8b5ef5cfd2
sleep3r <sleep3r@icloud.com>
2021-11-07 23:55:46 +0300

    Deploy demo

--- demo/app.py
+++ demo/app.py
@@ -10,18 +10,21 @@ st.title('GSBxSber Hackathon Demo')
 st.write('''Realtime scoring API default prediction model''')
 
 
-def user_input_features():
-    st.sidebar.header('Company features')
-
+def fin_input():
     input_features = {}
-    input_features['model_type'] = st.sidebar.selectbox("Model type:", ['fin', 'no_fin'])
+    form = st.sidebar.form(key="fin_form")
+    input_features["ul_capital_sum"] = form.number_input('ul_capital_sum', 0, step=1)
+    input_features["ul_founders_cnt"] = form.number_input('ul_founders_cnt', 0, step=1)
+    submit = form.form_submit_button('Get predictions')
+    return [input_features], submit
 
-    if input_features['model_type'] == "fin":
-        input_features["ul_capital_sum"] = st.sidebar.number_input('ul_capital_sum', 0, step=1)
-        input_features["ul_founders_cnt"] = st.sidebar.number_input('ul_founders_cnt', 0, step=1)
-    elif input_features['model_type'] == "no_fin":
-        input_features["ul_capital_sum"] = st.sidebar.number_input('ul_capital_sum', 0, step=1)
-    return [input_features]
+
+def no_fin_input():
+    input_features = {}
+    form = st.sidebar.form(key="no_fin_form")
+    input_features["ul_capital_sum"] = form.number_input('ul_capital_sum', 0, step=1)
+    submit = form.form_submit_button('Get predictions')
+    return [input_features], submit
 
 
 def st_shap(plot, height=None):
@@ -29,24 +32,30 @@ def st_shap(plot, height=None):
     components.html(shap_html, height=height)
 
 
-# explain model prediction results
-def explain_model_prediction():
+@st.cache(suppress_st_warning=True, show_spinner=False)
+def explain_model():
     # Calculate Shap values
     X, y = shap.datasets.diabetes()
     model = RandomForestClassifier()
     model.fit(X, y)
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(X)
+    return explainer, shap_values, X
+
+
+explainer, shap_values, X = explain_model()
+
+st.sidebar.header('Company features')
+model_type = st.sidebar.selectbox("Model type:", ['fin', 'no_fin'])
+if model_type == "fin":
+    json_data, submit = fin_input()
+else:
+    json_data, submit = no_fin_input()
+
+if submit:
+    st.subheader('Model Prediction Interpretation Plot')
     p = shap.force_plot(explainer.expected_value[1], shap_values[1], X)
-    return p, shap_values, X
-
-
-json_data = user_input_features()
-
-submit = st.sidebar.button('Get predictions')
-p, shap_values, X = explain_model_prediction()
-st.subheader('Model Prediction Interpretation Plot')
-st_shap(p, height=500)
+    st_shap(p, height=500)
 
 st.subheader('Summary Plot 1')
 fig, ax = plt.subplots(nrows=1, ncols=1)
