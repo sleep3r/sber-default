8b54bda2451e782547e91dc211f3ec4f654c307e
sleep3r <sleep3r@icloud.com>
2021-11-09 23:57:39 +0300

    Deploy demo

--- demo/download-models.sh
+++ demo/download-models.sh
@@ -1,2 +1,2 @@
 gdown https://drive.google.com/drive/folders/1adN_npoHO1A3TEF2kM2IkCWsbrTB38ll -O ~/checkpoints/fin --folder & \
-gdown https://drive.google.com/drive/folders/1UrCh6Z2ijsW07mmHV9dikq_X231g9GdV -O ~/checkpoints/no_fin --folder && fg
+gdown https://drive.google.com/drive/folders/1UrCh6Z2ijsW07mmHV9dikq_X231g9GdV -O ~/checkpoints/no_fin --folder
